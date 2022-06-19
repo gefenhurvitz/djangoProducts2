8c3cf11269282f6eacb9934926341fa879ac76a2
gefen <gh.production.isr@gmail.com>
2022-06-19 15:31:31 +0300

    first commit

---
+++ base/product.py
@@ -0,0 +1,12 @@
+products = [
+    {
+        "id": 1,
+        "price": 233,
+        "desc": "8K oled TV"
+    },
+    {
+        "id": 2,
+        "price": 33,
+        "desc": "router"
+    }
+]
